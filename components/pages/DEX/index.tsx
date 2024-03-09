20e07f226592d2da62055702e1c0f2c1ee64e794
tarifalhasan <tarifalhasan999@gmail.com>
2024-03-09 10:19:07 +0600

    DEX layout fixed

--- components/pages/DEX/index.tsx
+++ components/pages/DEX/index.tsx
@@ -1,13 +1,16 @@
 import Header from "@/components/layout/Header";
-
+import graph from "@/public/images/dex_graph.webp";
+import Image from "next/image";
 const DEX = () => {
   return (
-    <div className=" w-full xl:w-[81%] h-full flex gap-10">
-      <div className="p-2  rounded-[7px]   lg:pt-0 pl-0  flex-1 flex flex-col relative z-10 will-change-transform justify-start h-[calc(100vh-40px)]  w-full xl:w-[62%]">
+    <div className=" w-full xl:w-[calc(80%-20px)] h-full flex gap-5">
+      <div className="p-2  overflow-x-hidden overflow-y-auto rounded-[7px]   lg:pt-0   flex-1 flex flex-col relative z-10 will-change-transform justify-start h-[calc(100vh-40px)]  w-full xl:w-[calc(53%-20px)]">
         <Header />
-        <h2>DEX</h2>
+        <div>
+          <Image className="w-full" src={graph} alt="dex graph" />
+        </div>
       </div>
-      <aside className="flex-col  rounded-[7px] justify-between right-0 left-0 top-0 overflow-hidden overflow-y-auto bg-card flex-none max-w-[280px] relative w-[19%] z-50 h-full hidden xl:flex">
+      <aside className="flex-col p-4 rounded-[7px] justify-between right-0 left-0 top-0 overflow-hidden overflow-y-auto bg-card max-w-[280px]  relative w-[calc(27%-20px)] z-50 h-full hidden xl:flex">
         sdsds
       </aside>
     </div>
